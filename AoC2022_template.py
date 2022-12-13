e1eb5aeedbc3371e4512a3d12d9797b058dd08f1
Ben Simpson <blsimpson@mykeiki.com>
2022-12-12 17:24:50 -0800

    Day 11 and 12

---
+++ AoC2022_template.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+with open(".\day_XX\input.txt", 'r') as f:
+    lines = f.readlines()
+    
+part1 = ""
+print(f'Part 1: {part1}')
+
+part2 = ""
+print(f'Part 2: {part2}')
